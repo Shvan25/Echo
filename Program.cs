5cdfa6c655f44e4c9f1191ec5434ab76a234b034
Name <vbrfqlj@mail.ru>
2025-02-15 17:50:33 +0300

    Добавление цвета

--- Program.cs
+++ Program.cs
@@ -22,7 +22,9 @@ class MainClass
         {
             modif = modif.Remove(0, 2);
         }
+        Console.BackgroundColor = (ConsoleColor)deep;
         Console.WriteLine("..." + modif);
+
         if (deep > 1)
         {
             Echo(modif, deep - 1);
